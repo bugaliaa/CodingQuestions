ea4f47a3ceb6d7f6cd4e5b8cfdf4dc3aed84ce81
Siddhant Bugalia <sidbugalia@gmail.com>
2021-02-02 23:58:34 +0530

    Trailing Zeroes in Factorial

--- InterviewBit/Maths/TrailingZeroesInFact.java
+++ InterviewBit/Maths/TrailingZeroesInFact.java
@@ -1,7 +1,17 @@
 package InterviewBit.Maths;
 
+import java.util.Scanner;
+
 public class TrailingZeroesInFact {
-    public int trailingZeroes(int A) {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        sc.close();
+        System.out.println(trailingZeroes(A));
+    }
+
+    public static int trailingZeroes(int A) {
         int res = 0;
         for(int i = 5; i <= A; i *= 5){
             res += A/i;
