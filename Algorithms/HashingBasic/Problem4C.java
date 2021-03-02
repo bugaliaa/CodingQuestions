6a44378024ae89c148b9e38afbdf8c7146588993
Siddhant Bugalia <sidbugalia@gmail.com>
2021-03-02 14:09:33 +0530

    Problem-4C Registration System (CodeForces)

---
+++ Algorithms/HashingBasic/Problem4C.java
@@ -0,0 +1,24 @@
+package Algorithms.HashingBasic;
+
+import java.util.*;
+import java.io.*;
+
+// https://codeforces.com/problemset/problem/4/C
+
+public class Problem4C {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Map<String, Integer> map = new HashMap<>();
+        while(n-- > 0){
+            String s = br.readLine();
+            if(map.containsKey(s)){
+                System.out.println(s+map.get(s));
+                map.replace(s, map.get(s)+1);
+            }else{
+                System.out.println("OK");
+                map.put(s, 1);
+            }
+        }
+    }
+}
