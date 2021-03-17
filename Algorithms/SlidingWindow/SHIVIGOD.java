4bd7c151b36d282a7941fc2370f7c62d353f37a4
Siddhant Bugalia <sidbugalia@gmail.com>
2021-03-17 15:42:34 +0530

    Shivigawdz SHIVIGOD (CodeChef)

--- Algorithms/SlidingWindow/SHIVIGOD.java
+++ Algorithms/SlidingWindow/SHIVIGOD.java
@@ -8,19 +8,27 @@ public class SHIVIGOD {
         int t = sc.nextInt();
         while(t-- > 0){
             int n = sc.nextInt();
-            long[] ar = new long[n];
+            double[] ar = new double[n];
             int b = sc.nextInt();
             int a = sc.nextInt();
-            Deque<Long> dq = new LinkedList<>();
-            long sum = 0;
-            for(int i = 0; i < a; i++){
-                sum += ar[i];
-                dq.add(ar[i]);
-            }
-            double avg = (double)sum/a;
-            List<Integer> list = new ArrayList<>();
-            
+            double avg = 0.0;
 
+            for(int i = 0; i < n; i++){
+                ar[i] = sc.nextLong();
+            }
+            
+            for(int i = a; i <= b; i++){
+                double temp = 0;
+                for(int j = 0; j < i; j++) temp += ar[j];
+
+                avg = Math.max(avg, temp/i);
+                for(int j = i; j < n; j++){
+                    temp += ar[j];
+                    temp -= ar[j-i];
+                    avg = Math.max(avg, temp/i);
+                }
+            }
+            System.out.println(avg);
         }
         sc.close();
     }
