499f464d5d729b448b639b4b06a0e90ee7ab8fa7
Siddhant Bugalia <sidbugalia@gmail.com>
2021-03-26 22:54:11 +0530

    Unique Paths WITHOUT DP (LeetCode)

---
+++ SDE/Day3/Problem6.java
@@ -0,0 +1,16 @@
+package SDE.Day3;
+
+public class Problem6 {
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(3, 7));
+    }
+    public static int uniquePaths(int m, int n) {
+        int N = m + n - 2;
+        int r = m - 1;
+        double res = 1;
+        for(int i = 1; i <= r; i++){
+            res = res * (N - r + i)/i;
+        }
+        return (int)res;
+    }
+}
