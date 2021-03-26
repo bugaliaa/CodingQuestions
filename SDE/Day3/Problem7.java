74dc3f498d44b4d45c952d086aa8c2486f5341ad
Siddhant Bugalia <sidbugalia@gmail.com>
2021-03-26 23:50:13 +0530

    Reverse Pairs HARD (Leetcode)

--- SDE/Day3/Problem7.java
+++ SDE/Day3/Problem7.java
@@ -1,10 +1,48 @@
 package SDE.Day3;
 
+import java.util.*;
+
+//https://leetcode.com/problems/reverse-pairs/
+//Reverse Pairs
+
 public class Problem7 {
     public static void main(String[] args) {
-        
+        int[] nums = {1,3,2,3,1};
+        System.out.println(reversePairs(nums));
     }
     public static int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length-1);
+    }
+    public static int mergeSort(int[] nums, int low, int high){
+        if(low >= high) return 0;
+        int mid = (low+high)/2;
+        int inv = mergeSort(nums, low, mid);
+        inv += mergeSort(nums, mid+1, high);
+        inv += merge(nums, low, mid, high);
+        return inv;
+    }
+    public static int merge(int[] nums, int low, int mid, int high){
+        int count = 0;
+        int j = mid+1;
+        for(int i = low; i <= mid; i++){
+            while(j <= high && nums[i] > 2*(long)nums[j]) j++;
+            count += (j - (mid+1));
+        }
+        List<Integer> temp = new ArrayList<>();
+        int left = low, right = mid+1;
+        while(left <= mid && right <= high){
+            if(nums[left] <= nums[right]){
+                temp.add(nums[left++]);
+            }else{
+                temp.add(nums[right++]);
+            }
+        }
+        while(left <= mid) temp.add(nums[left++]);
+        while(right <= high) temp.add(nums[right++]);
         
+        for(int i = low; i <= high; i++){
+            nums[i] = temp.get(i-low);
+        }
+        return count;
     }
 }
