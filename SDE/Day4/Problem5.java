f2d5789659820389a57b9ea1a27a2d21f4655d26
Siddhant Bugalia <sidbugalia@gmail.com>
2021-04-12 14:46:29 +0530

    Count the numcer of subarrays testing

---
+++ SDE/Day4/Problem5.java
@@ -0,0 +1,13 @@
+package SDE.Day4;
+
+// https://www.geeksforgeeks.org/count-number-subarrays-given-xor/
+// Count the number of subarrays having a given XOR
+
+public class Problem5 {
+    public static void main(String[] args) {
+        
+    }
+    public static int subarrayXOR(int[] a, int k){
+        
+    }
+}
