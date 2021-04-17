cc2dfd516f6510f621b68862183c199a67e2cb78
Siddhant Bugalia <sidbugalia@gmail.com>
2021-04-17 14:22:36 +0530

    Necessarry Classes Node and ListNode

---
+++ SDE/Classes/Node.java
@@ -0,0 +1,13 @@
+package SDE.Classes;
+
+public class Node {
+    int data;
+    Node next;
+    Node bottom;
+        
+	Node(int d){
+        data = d;
+        next = null;
+        bottom = null;
+    }
+}
