2829ca7d8e3079887bcba1ef39d145d16a8f001f
Siddhant Bugalia <sidbugalia@gmail.com>
2021-05-14 13:46:43 +0530

    Stack implementation using 2 Qs (Push Costly)

---
+++ SDE/Day13/Problem5.java
@@ -0,0 +1,28 @@
+package SDE.Day13;
+
+import java.util.*;
+
+// Implementation of Stack using 2 Queues Part1 (usisng push costly)
+
+public class Problem5 {
+    Queue<Integer> q1 = new LinkedList<>();
+    Queue<Integer> q2 = new LinkedList<>();
+    public void push(int data){
+        q2.add(data);
+        while(!q1.isEmpty()){
+            q2.add(q1.peek());
+            q1.poll();
+        }
+        Queue<Integer> q = q1;
+        q1 = q2;
+        q2 = q;
+    }
+    public void pop(){
+        if(q1.isEmpty()) return;
+        q1.poll();
+    }
+    public int peek(){
+        if(q1.isEmpty()) return -1;
+        return q1.peek();
+    }
+}
