2d4ace7713c31e9cfbdb2b1b1760305029075764
Siddhant Bugalia <sidbugalia@gmail.com>
2021-05-19 15:59:09 +0530

    Longest Common Subsequence (LeetCode)

---
+++ SDE/Day16/Problem4.java
@@ -0,0 +1,27 @@
+package SDE.Day16;
+
+// https://leetcode.com/problems/longest-common-subsequence/
+// Longest Common Subsequence (LeetCode)
+
+public class Problem4 {
+    public static void main(String[] args) {
+        String text1 = "abcde";
+        String text2 = "ace";
+        System.out.println(longestCommonSubsequence(text1, text2));
+    }
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < m+1; j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
