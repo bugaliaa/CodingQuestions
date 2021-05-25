05338831c4d74f6c7c9597e419a0d1eadcef1b78
Siddhant Bugalia <sidbugalia@gmail.com>
2021-05-25 08:40:37 +0530

    Minimum Depth of a Binary Tree (Iterative) (LeetCode)

--- SDE/Day18/Problem4.java
+++ SDE/Day18/Problem4.java
@@ -1,6 +1,7 @@
 package SDE.Day18;
 
 import SDE.Classes.LeetCode.TreeNode;
+import java.util.*;
 
 // https://leetcode.com/problems/minimum-depth-of-binary-tree/
 // Minimum Depth of binary tree (LeetCode)
@@ -8,8 +9,20 @@ import SDE.Classes.LeetCode.TreeNode;
 public class Problem4 {
     public int minDepth(TreeNode root) {
         if(root == null) return 0;
-        int left = minDepth(root.left);
-        int right = minDepth(root.right);
-        return (left == 0 || right == 0) ? left+right+1 : Math.min(left, right)+1;
+        Queue<TreeNode> q = new LinkedList<>();
+        int level = 1;
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            while(size-- > 0){
+                TreeNode node = q.poll();
+                if(node.left == null && node.right == null) return level;
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+            }
+            level++;
+        }
+
+        return level;
     }
 }
