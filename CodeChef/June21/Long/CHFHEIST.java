33c8f16eb6278d625c0c3ff3900222876b00e2d4
Siddhant Bugalia <sidbugalia@gmail.com>
2021-06-04 20:21:59 +0530

    Bella Ciao CHFHEIST

---
+++ CodeChef/June21/Long/CHFHEIST.java
@@ -0,0 +1,19 @@
+package CodeChef.June21.Long;
+
+import java.util.Scanner;
+
+public class CHFHEIST {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int D = sc.nextInt();
+            int d = sc.nextInt();
+            int P = sc.nextInt();
+            int Q = sc.nextInt();
+
+            
+        }
+        sc.close();
+    }
+}
