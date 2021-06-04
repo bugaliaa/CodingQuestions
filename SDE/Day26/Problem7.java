a5a1d0f12f5169dc83458321eb08a8cd1f3278d4
Siddhant Bugalia <sidbugalia@gmail.com>
2021-06-04 09:35:00 +0530

    Word Break (LeetCode)

---
+++ SDE/Day26/Problem7.java
@@ -0,0 +1,27 @@
+package SDE.Day26;
+
+// https://leetcode.com/problems/word-break/
+// Word Break (LeetCode)
+
+import java.util.*;
+
+public class Problem7 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+
+        int n = s.length();
+        boolean dp[] = new boolean[n+1];
+        dp[0] = true;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j < i; j++){
+                if(dp[j] && set.contains(s.substring(j, i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
