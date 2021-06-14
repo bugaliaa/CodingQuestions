12b8d5345a07767d6a5aec542009e6331bc5bff0
Siddhant Bugalia <sidbugalia@gmail.com>
2021-06-14 09:35:26 +0530

    Insertion sort list

--- InterviewBit/LinkedList/ListSort/Problem3.java
+++ InterviewBit/LinkedList/ListSort/Problem3.java
@@ -1,5 +1,34 @@
 package InterviewBit.LinkedList.ListSort;
 
+// https://www.interviewbit.com/problems/insertion-sort-list/
+// Insertion Sort List
+
 public class Problem3 {
-    
+    class ListNode{
+        public int val;
+        public ListNode next;
+        ListNode(int x){val = x; next = null;}
+    }
+    public ListNode insertionSortList(ListNode A) {
+        ListNode start = new ListNode(0);
+        ListNode head = A;
+        start.next = head;
+
+        ListNode i = head;
+        ListNode prev = start;
+
+        while(i != null){
+            if(i.next != null && (i.val > i.next.val)){
+                while(prev.next != null && (prev.next.val < i.next.val)) prev = prev.next;
+
+                ListNode temp = prev.next;
+                prev.next = i.next;
+                i.next = i.next.next;
+                prev.next.next = temp;
+                prev = start;
+            }else i = i.next;
+        }
+
+        return start.next;
+    }
 }
