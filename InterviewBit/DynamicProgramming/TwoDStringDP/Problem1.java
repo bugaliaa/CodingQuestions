3e0b8c0808a912a381b96701785bb6f0e5712bd8
Siddhant Bugalia <sidbugalia@gmail.com>
2021-07-08 16:22:05 +0530

    Longest Common Subsequence

---
+++ InterviewBit/DynamicProgramming/TwoDStringDP/Problem1.java
@@ -0,0 +1,22 @@
+package InterviewBit.DynamicProgramming.TwoDStringDP;
+
+// https://www.interviewbit.com/problems/longest-common-subsequence/
+// Longest Common Subsequence
+
+public class Problem1 {
+    public int solve(String A, String B) {
+        int n = A.length();
+        int m = B.length();
+
+        int dp[][] = new int[n+1][m+1];
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(A.charAt(i-1) == B.charAt(j-1)) dp[i][j] = dp[i-1][j-1]+1;
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[n][m];
+    }
+}
