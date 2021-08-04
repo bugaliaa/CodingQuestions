e4120f183452fcebe06e106cb09399942a801b3c
Siddhant Bugalia <sidbugalia@gmail.com>
2021-08-04 22:15:21 +0530

    Maximum Occurences (Simple)

--- CPList/HashingBasic/MaximumOccurence.java
+++ CPList/HashingBasic/MaximumOccurence.java
@@ -1,33 +1,30 @@
 package CPList.HashingBasic;
 
-import java.io.*;
 import java.util.*;
-import java.util.Map.Entry;
 
 // https://www.hackerearth.com/practice/data-structures/hash-tables/basics-of-hash-tables/practice-problems/algorithm/maximum-occurrence-9/
 
 public class MaximumOccurence {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        br.close();
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-        Map<Character, Integer> map = new HashMap<>();
-        for(char c: s.toCharArray()){
-            if(map.containsKey(c)){
-                map.replace(c, map.get(c)+1);
-            }else{
-                map.put(c, 1);
+        String s = sc.nextLine();
+        sc.close();
+        int table[] = new int[128];
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            table[(int)s.charAt(i)]++;
+        }
+
+        int max = Integer.MIN_VALUE;
+        int idx = -1;
+        for(int i = 0; i < 128; i++){
+            if(table[i] > max){
+                max = table[i];
+                idx = i;
             }
         }
-        Entry<Character, Integer> max = null;
-        for(Entry<Character, Integer> e: map.entrySet()){
-            if(max == null || e.getValue().compareTo(max.getValue()) > 0){
-                max = e;
-            }else if(e.getValue().compareTo(max.getValue()) == 0 && (int)e.getKey() < (int)max.getKey()){
-                max = e;
-            }
-        }
-        System.out.println(max.getKey() + " " + max.getValue());
+        char c = (char)(idx);
+        System.out.print(c + " " + max);
     }
 }
