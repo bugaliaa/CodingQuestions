f55219852a3dd4d0b1b82b48c322b3e4ef3e8e8b
Siddhant Bugalia <sidbugalia@gmail.com>
2021-08-15 17:56:32 +0530

    ECAPR206 Codechef

--- CPList/SlidingWindow/ECAPR206.java
+++ CPList/SlidingWindow/ECAPR206.java
@@ -1,7 +1,5 @@
 package CPList.SlidingWindow;
 
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class ECAPR206 {
@@ -12,27 +10,22 @@ public class ECAPR206 {
             int n = sc.nextInt();
             int k = sc.nextInt();
             int a[] = new int[n];
-            int max_sum = 0;
-            int sum = 0;
-            Deque<Integer> dq = new LinkedList<>();
+            long max = 0;
+            long sum = 0;
+            
             for(int i = 0; i < n; i++){ 
                 a[i] = sc.nextInt();
             }
             for(int i = 0; i < k; i++){
-                dq.add(a[i]);
                 sum += a[i];
             }
-            max_sum = sum;
-            int i = k;
-            while(i%n != k-1){
-                sum -= dq.getFirst();
-                dq.removeFirst();
-                dq.add(a[i%n]);
-                sum += a[i%n];
-                max_sum = Math.max(max_sum, sum);
-                i++;
+            max = sum;
+            for(int i = 1; i < n; i++){
+                sum -= a[i-1];
+                sum += a[(i+k-1)%n];
+                max = Math.max(max, sum);
             }
-            System.out.println(max_sum);
+            System.out.println(max);
         }
         sc.close();
     }
