aa8583bac6ca7b6d1d611377f61a92550db9beb5
Siddhant Bugalia <sidbugalia@gmail.com>
2021-11-08 21:16:34 +0530

    Problem 1201 Zero Array (CodeForces)

--- CPList/GreedyAlgorithms/Problem1201B.java
+++ CPList/GreedyAlgorithms/Problem1201B.java
@@ -2,7 +2,7 @@ package CPList.GreedyAlgorithms;
 
 import java.util.*;
 
-public class Problem1201 {
+public class Problem1201B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
