208adda49712f059b22543e4e7aaf5c928101cad
Siddhant Bugalia <sidbugalia@gmail.com>
2022-01-10 19:22:01 +0530

    Problem 274A (Codeforces)

---
+++ CPList/GreedyAlgorithms/Problem274A.java
@@ -0,0 +1,19 @@
+package CPList.GreedyAlgorithms;
+import java.util.*;
+
+public class Problem274A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int a[] = new int[n];
+        for(int i = 0; i < n; i++) a[i] = sc.nextInt();
+
+        sc.close();
+
+        Arrays.sort(a);
+
+        
+    }
+}
